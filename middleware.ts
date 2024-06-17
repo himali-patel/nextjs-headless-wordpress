8e64f9e6e163309aaa25aad2d773d4ddf4b672b4
Himali Patel <45478946+himali-patel@users.noreply.github.com>
2024-06-17 15:39:08 +0530

    updated config file

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,15 @@ export const middleware: NextMiddleware = ( req: NextRequest ) => {
 	// https://github.com/vercel/next.js/pull/45772
 	// https://github.com/vercel/next.js/blob/v13.1.1/packages/next/server/base-server.ts#L1069
 	const headers = new Headers(req.headers);
-  headers.delete('x-middleware-prefetch');
+   // Add new request headers
+   headers.set('x-hello-from-middleware1', 'hello')
+   headers.set('x-hello-from-middleware2', 'world!')
+ 
+   // Update an existing request header
+   headers.set('user-agent', 'New User Agent overriden by middleware!')
+ 
+   // Delete an existing request header
+   headers.delete('x-from-client')
 
 	// Required health check endpoint on VIP. Do not remove.
 	if ( req.nextUrl.pathname === '/cache-healthcheck' ) {
